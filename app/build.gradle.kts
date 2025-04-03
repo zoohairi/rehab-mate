58e05ccd5d1b458130f18ef468a420a5f44dfb5d
naddy1312 <putri.nadrah02@gmail.com>
2025-04-03 10:04:49 +0800

    Watch Items

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
 
     // Machine learning
     implementation("com.microsoft.onnxruntime:onnxruntime-android:1.21.0")
+    implementation(libs.play.services.wearable)
 
     // Testing libraries
     testImplementation(libs.junit)
